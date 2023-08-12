148fd1b3305880a448870045ca19080d4d0f6df1
SAMBIT CHAKRABORTY <smbtchakraborty@gmail.com>
2023-08-13 01:28:19 +0530

    Add: Auth0 Basic setup

--- server.js
+++ server.js
@@ -1,26 +1,10 @@
-'use strict';
+// server.js or app.js
+
 const express = require('express');
-const auth = require('./auth.js');
+const authRouter = require('./auth.js');
 
 const app = express();
 
-const http = require('http').createServer(app);
-//const io = require('socket.io')(http);
+app.use('/', authRouter);
 
-
-app.set('views', './views');
-
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
-});
-
-// Port
-const PORT = process.env.PORT || 2000;
-http.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-});
+app.listen(8080, () => console.log("Server started on port 8080"));
