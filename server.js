f94651a9c20c86667f54efd7a14b2d1506f5289a
Sayandeep <sayandeep11@gmail.com>
2023-08-13 01:23:32 +0530

    Final commit minor changes in contract willbe done later

--- server.js
+++ server.js
@@ -8,17 +8,33 @@ app.get("/pdetails", (req, res) => {
 app.get("/pregister", (req, res) => {
     res.sendFile(path.join(__dirname + "/setPatientsdetails.html"));
 })
+app.get("/pedit", (req, res) => {
+    res.sendFile(path.join(__dirname + "/editpatients.html"));
+})
 app.get("/dview", (req, res) => {
     res.sendFile(path.join(__dirname + "/viewdoc.html"));
 })
 app.get("/dset", (req, res) => {
     res.sendFile(path.join(__dirname + "/setDoc.html"));
 })
+app.get("/dedit", (req, res) => {
+    res.sendFile(path.join(__dirname + "/editdoc.html"));
+})
 app.get("/aview", (req, res) => {
     res.sendFile(path.join(__dirname + "/appointmentview.html"));
 })
-app.get("/dedit", (req, res) => {
-    res.sendFile(path.join(__dirname + "/editdoc.html"));
+
+app.get("/aset", (req, res) => {
+    res.sendFile(path.join(__dirname + "/setappointment.html"));
+})
+app.get("/aupdate", (req, res) => {
+    res.sendFile(path.join(__dirname + "/updateappointment.html"));
+})
+app.get("/givepermi", (req, res) => {
+    res.sendFile(path.join(__dirname + "/givepermission.html"));
+})
+app.get("/revokepermi", (req, res) => {
+    res.sendFile(path.join(__dirname + "/revokepermi.html"));
 })
 const server = app.listen(6988);
 const portNumber = server.address().port;
