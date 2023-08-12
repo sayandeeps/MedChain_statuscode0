391368603f16bddd938fdb4239f05629373da4c5
Sayandeep <sayandeep11@gmail.com>
2023-08-12 20:29:54 +0530

    Added: doc reg view doc edit doc appointment view

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ app.get("/pdetails", (req, res) => {
 app.get("/pregister", (req, res) => {
     res.sendFile(path.join(__dirname + "/setPatientsdetails.html"));
 })
+app.get("/dview", (req, res) => {
+    res.sendFile(path.join(__dirname + "/viewdoc.html"));
+})
+app.get("/dset", (req, res) => {
+    res.sendFile(path.join(__dirname + "/setDoc.html"));
+})
+app.get("/aview", (req, res) => {
+    res.sendFile(path.join(__dirname + "/appointmentview.html"));
+})
+app.get("/dedit", (req, res) => {
+    res.sendFile(path.join(__dirname + "/editdoc.html"));
+})
 const server = app.listen(6988);
 const portNumber = server.address().port;
 console.log(`port is open on ${portNumber}`);
